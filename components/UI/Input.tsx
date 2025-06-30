d02dd038c36eb895f3243351fc8cfcf52be28bd0
aditya9336 <adityagupta3602@gmail.com>
2025-06-30 05:57:22 +0530

    fix: add explicit InputProps type to Input component

--- components/UI/Input.tsx
+++ components/UI/Input.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { InputHTMLAttributes, ForwardedRef } from 'react';
 
-export const Input = React.forwardRef(({ className = '', ...props }, ref) => (
-  <input
-    ref={ref}
-    className={`bg-white/60 backdrop-blur-glass rounded-lg border border-primary/20 px-4 py-2 focus:ring-2 focus:ring-primary focus:border-primary outline-none transition ${className}`}
-    {...props}
-  />
-));
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className = '', ...props }, ref: ForwardedRef<HTMLInputElement>) => (
+    <input
+      ref={ref}
+      className={`bg-white/60 backdrop-blur-glass rounded-lg border border-primary/20 px-4 py-2 focus:ring-2 focus:ring-primary focus:border-primary outline-none transition ${className}`}
+      {...props}
+    />
+  )
+);
 Input.displayName = 'Input'; 
